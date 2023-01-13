19105a6408a098281b636df572d38a004d246e00
PrannavP <ramsarilekhi@gmail.com>
2023-01-13 21:33:48 +0545

    Initial Commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+from credentials import my_email
+from credentials import my_password_key
+from credentials import receiver_email
+
+email = my_email
+password = my_password_key
+send_to_email = receiver_email
+subject = 'Testing'
+message = 'This is me testing stuffs hehe'
+
+msg = MIMEMultipart()
+msg['From'] = email
+msg['To'] = send_to_email
+msg['Subject'] = subject
+
+msg.attach(MIMEText(message, 'plain'))
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(email, password)
+text = msg.as_string()
+server.sendmail(email, send_to_email, text)
+server.quit()
